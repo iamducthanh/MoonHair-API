37fdf85473c6754bab68464a75ea1f2fe6f422a4
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2025-04-17 22:06:34 +0700

    Create MoonHair_Sql_update.sql

---
+++ MoonHair_Sql_update.sql
@@ -0,0 +1,51 @@
+create table phieu_nhap (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`ma_phieu` varchar(255),
+	`id_chi_nhanh` int NOT NULL,
+	`ngay_tao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	`ten_ncc` varchar(255),
+	`trang_thai` varchar(10),
+	`tong_tien` double DEFAULT NULL,
+	`giam_gia` double DEFAULT NULL,
+	`tong_tien_thanh_toan` double DEFAULT NULL,
+	`chi_phi_khac` double DEFAULT NULL,
+	`ghi_chu` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+);
+
+create table chi_tiet_phieu_nhap (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`id_phieu_nhap` int NOT NULL,
+    `id_san_pham` int NOT NULL,
+	 `so_luong` int not null,
+	`gia_nhap` DECIMAL(15,2) not null,
+	  
+    CONSTRAINT fk_ctpn_phieunhap FOREIGN KEY (id_phieu_nhap) REFERENCES phieu_nhap(id),
+    CONSTRAINT fk_ctpn_sanpham FOREIGN KEY (id_san_pham) REFERENCES san_pham(id)
+);
+  
+CREATE TABLE lo_hang (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `id_san_pham` int NOT NULL,
+    `id_chi_tiet_phieu_nhap` int, -- 1-1 với chi tiết phiếu nhập
+    `ma_lo` VARCHAR(100) NOT NULL,
+    `so_luong_nhap` INT NOT NULL,
+    `so_luong_con` INT NOT NULL,
+    `gia_von` DECIMAL(15,2) NOT NULL,
+        `gia_ban` DECIMAL(15,2) NOT NULL,      -- Giá bán theo lô
+    `ngay_nhap` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_lo_sanpham FOREIGN KEY (id_san_pham) REFERENCES san_pham(id),
+    CONSTRAINT fk_lo_ctpn FOREIGN KEY (id_chi_tiet_phieu_nhap) REFERENCES chi_tiet_phieu_nhap(id)
+);
+
+select sp.ten, lh.gia_ban, lh.so_luong_con from san_pham sp left join lo_hang lh on sp.id = lh.id_san_pham ;
+ALTER TABLE san_pham DROP COLUMN so_luong;
+
+INSERT INTO MoonHair.lo_hang
+(id, id_san_pham, id_chi_tiet_phieu_nhap, ma_lo, so_luong_nhap, so_luong_con, gia_von, ngay_nhap, gia_ban)
+VALUES(1, 1, 1, 'SP001-15042025-001', 15, 15, 200000.00, '2025-04-15 21:34:54', 400000.00);
+INSERT INTO MoonHair.lo_hang
+(id, id_san_pham, id_chi_tiet_phieu_nhap, ma_lo, so_luong_nhap, so_luong_con, gia_von, ngay_nhap, gia_ban)
+VALUES(3, 1, 1, 'SP001-15042025-002', 10, 10, 220000.0, '2025-04-15 21:34:54', 400000.0);
+INSERT INTO MoonHair.lo_hang
+(id, id_san_pham, id_chi_tiet_phieu_nhap, ma_lo, so_luong_nhap, so_luong_con, gia_von, ngay_nhap, gia_ban)
+VALUES(2, 2, 2, 'SP002-15042025-001', 13, 13, 300000.00, '2025-04-15 21:34:54', 500000.00);
